1c3d90d23a67d12794feb9772002dcfcbba1570a
joshua-noel <noelejoshua@gmail.com>
2022-05-23 05:36:02 -0400

    Can download youtube videos

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 #!/usr/bin/env python
 import discord
 import json
-from colorama import init, Fore, Style
-from YTDownloader import Download
+from rich.console import Console
+import YTDownloader as YT
 
 #Setup
 with open("token.json", "r") as f:
     token = json.load(f)
 
-init() #Initialize colorama
-client = discord.Client()
+console = Console() #rich console object
+client = discord.Client() #initalize discord bot
 
 @client.event
 async def on_connect():
-    print(Fore.GREEN + "Connected to discord's servers" + Style.RESET_ALL)
+    console.log("[green]Connected to discord's servers[/Green]")
 
 @client.event
 async def on_disconnect():
-    print(Fore.RED + "Disconnected from discord's servers" + Style.RESET_ALL)
+    console.log("[red]Disconnected from discord's servers[/red]")
 
 @client.event
 async def on_ready():
-    print(Fore.GREEN + "Logged in as {0.user}".format(client) + Style.RESET_ALL)
+    console.log("[green]Logged in as {0.user}[/green]".format(client))
 
 #Commands
+#List commands
 async def commands(message):
     with open("commands.txt", "r") as f:
         commands = f.read().splitlines()
         await message.channel.send("```{0}```".format("\n".join(commands)))
 
+#Download YT video
 async def download(message):
-    pass
-    #await message.channel.send(file= discord.File("videos/video.mp4"))
+    url = message.content[10:] #gets url from message
+    await message.delete(delay= None) #deletes command message
 
+    if YT.Downloader().download(url) == 0: #downloads video
+       await message.channel.send("Could not download video {0}".format(message.author.mention))
+        
+    else:
+        await message.channel.send("Downloaded {0}".format(message.author.mention), file= discord.File("videos/video.mp4")) #sends video to channel
+
+#Repeat user message
 async def repeat(message):
-    await message.channel.send("@{0} said {1}".format(message.author, message.content[7:]))
+    await message.channel.send("{0} said {1}".format(message.author.mention, message.content[7:]))
 
 #Command handling
 @client.event
