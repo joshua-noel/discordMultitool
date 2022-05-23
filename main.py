20e8cd2f9c56d0af645ee5a7abc5564088041c33
joshua-noel <noelejoshua@gmail.com>
2022-05-23 03:37:36 -0400

    saving work

--- main.py
+++ main.py
@@ -1,11 +1,54 @@
+#!/usr/bin/env python
 import discord
+import json
+from colorama import init, Fore, Style
+from YTDownloader import Download
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+#Setup
+with open("token.json", "r") as f:
+    token = json.load(f)
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+init() #Initialize colorama
+client = discord.Client()
 
-client = MyClient()
-client.run('my token goes here')
+@client.event
+async def on_connect():
+    print(Fore.GREEN + "Connected to discord's servers" + Style.RESET_ALL)
+
+@client.event
+async def on_disconnect():
+    print(Fore.RED + "Disconnected from discord's servers" + Style.RESET_ALL)
+
+@client.event
+async def on_ready():
+    print(Fore.GREEN + "Logged in as {0.user}".format(client) + Style.RESET_ALL)
+
+#Commands
+async def commands(message):
+    with open("commands.txt", "r") as f:
+        commands = f.read().splitlines()
+        await message.channel.send("```{0}```".format("\n".join(commands)))
+
+async def download(message):
+    pass
+    #await message.channel.send(file= discord.File("videos/video.mp4"))
+
+async def repeat(message):
+    await message.channel.send("@{0} said {1}".format(message.author, message.content[7:]))
+
+#Command handling
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("&commands"):
+        await commands(message)
+
+    if message.content.startswith("&download"):
+        await download(message)
+
+    if message.content.startswith("&repeat"):
+        await repeat(message)
+
+client.run(token['token'])
