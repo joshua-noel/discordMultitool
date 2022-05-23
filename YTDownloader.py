2dbec47a74c8c570dba9daa9b507b4c23f228317
joshua-noel <noelejoshua@gmail.com>
2022-05-23 04:23:20 -0400

    YT downloader module

--- YTDownloader.py
+++ YTDownloader.py
@@ -1,6 +1,42 @@
-class Downloader:
-    def __init__(self, url):
-        self.url = url
+#!/usr/bin/env python
+import pytube
+from pytube import YouTube
+import os
 
-    def download(self):
+class Downloader:
+    def __init__(self):
         pass
+
+    def download(self, url):
+        #exception handling
+        try:
+            yt = YouTube(str(url))
+
+        except pytube.exceptions.RegexMatchError:
+            print("Invalid URL")
+            return 0
+
+        except pytube.exceptions.VideoUnavailable:
+            print("Video unavailable")
+            return 0
+
+        except pytube.exceptions.ExtractError:
+            print("Error extracting video")
+            return 0
+        
+        except pytube.exceptions.HTMLParseError:
+            print("Error parsing HTML")
+            return 0
+
+        except pytube.exceptions.LiveStreamError(url):
+            print("Live stream")
+            return 0
+
+        except pytube.exceptions.MaxRetriesExceeded:
+            print("Max retries exceeded")
+            return 0
+        
+        else:    
+            print("Dowmlading {}".format(yt.title))    
+            stream = yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
+            stream.download(os.getcwd() + "/videos/", filename= "video.mp4")
