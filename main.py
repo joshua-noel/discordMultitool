7ded1b07c30f5e25fa17fe40251587b9eb94bf3a
joshua-noel <noelejoshua@gmail.com>
2022-05-24 17:33:18 -0400

    Cog organisation

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 import discord
 from discord.ext import commands
 import json
-from rich.console import Console
-import YTDownloader as YT
+import os
 
 #Setup
 #loads config
@@ -13,74 +12,23 @@ with open("config.json", "r") as f:
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix= config["prefix"], intents=intents)
 
-console = Console() #rich console object
+#Cog loading/Unloading
+@bot.command
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
 
-#Events
-#Bot Events
-@bot.event
-async def on_connect():
-    console.log("[green]Connected to discord's servers[/Green]")
+@bot.command
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
 
-@bot.event
-async def on_disconnect():
-    console.log("[red]Disconnected from discord's servers[/red]")
+@bot.command
+async def reload(ctx, extension):    
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
 
-@bot.event
-async def on_resumed():
-    console.log("[green]Resumed connection to discord's servers[/green]")
-
-@bot.event
-async def on_ready():
-    console.log("[green]Logged in as {0.user}[/green]".format(bot))
-    await bot.change_presence(activity= discord.Game(name= "&commands")) #sets bot's status
-
-#General Events
-@bot.event
-async def on_member_join(member):
-    console.print("[green]{0.name} has joined the server[/green]".format(member))
-    await member.send("Welcome {0.mention} to the server!".format(member))
-    await member.send("Type '&commands' to see a list of commands!")
-
-@bot.event
-async def on_member_remove(member):
-    console.print("[red]{0.name} has left the server[/red]".format(member))
-    await member.send("Goodbye {0.mention}! Hope to see you again!".format(member))
-
-#Admin Events
-@bot.event
-async def on_member_ban(guild, member):
-    console.print("[red]{0.name} has been banned[/red]".format(member))
-    await member.send("You have been banned from the server!")
-
-@bot.event
-async def on_member_unban(guild, member):
-    console.print("[green]{0.name} has been unbanned[/green]".format(member))
-    await member.send("You have been unbanned from the server!")
-
-#Commands
-#List commands
-@bot.command(name= "commands")
-async def commands(ctx):
-    with open("commands.txt", "r") as f:
-        commands = f.read().splitlines()
-        await ctx.send("```{0}```".format("\n".join(commands)))   
-
-#Download YT video
-@bot.command(name= "download")
-async def download(ctx, url):
-    await ctx.message.delete(delay= None) #deletes command message
-    await ctx.send("Attempting to download your video", delete_after= 5) #limits channel spam
-
-    if YT.Downloader().download(url) == 0: #downloads video
-        await ctx.send("Could not download video {0}".format(ctx.message.author.mention))
+#Load all cogs on startup
+for file in os.listdir('./cogs'):
+    if file.endswith('.py'):
+        bot.load_extension(f'cogs.{file[:-3]}')
         
-    else:
-        #upload size limit exception handling
-        try:
-            await ctx.send("Downloaded {0}".format(ctx.message.author.mention), file= discord.File("videos/video.mp4")) #sends video to channel
-
-        except discord.errors.HTTPException:
-            await ctx.send("Could not send video {0}".format(ctx.message.author.mention))
-            console.print("[red]Could not send video {0}[/red]".format(ctx.message.author.mention))
-
 bot.run(config['token'])
