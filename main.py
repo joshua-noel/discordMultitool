259c63d517a20c43292ae15509ae355efc23be20
joshua-noel <noelejoshua@gmail.com>
2022-05-23 22:51:10 -0400

    Un/Ban & Join/Leave event tracking

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ bot = commands.Bot(command_prefix= config["prefix"], intents=intents)
 console = Console() #rich console object
 
 #Events
+#Bot statuses
 @bot.event
 async def on_connect():
     console.log("[green]Connected to discord's servers[/Green]")
@@ -25,8 +26,36 @@ async def on_disconnect():
     console.log("[red]Disconnected from discord's servers[/red]")
 
 @bot.event
+async def on_resumed():
+    console.log("[green]Resumed connection to discord's servers[/green]")
+
+@bot.event
 async def on_ready():
     console.log("[green]Logged in as {0.user}[/green]".format(bot))
+    await bot.change_presence(activity= discord.Game(name= "&commands")) #sets bot's status
+
+#General Events
+@bot.event
+async def on_member_join(member):
+    console.print("[green]{0.name} has joined the server[/green]".format(member))
+    await member.send("Welcome {0.mention} to the server!".format(member))
+    await member.send("Type '&commands' to see a list of commands!")
+
+@bot.event
+async def on_member_remove(member):
+    console.print("[red]{0.name} has left the server[/red]".format(member))
+    await member.send("Goodbye {0.mention}! Hope to see you again!".format(member))
+
+#Admin Events
+@bot.event
+async def on_member_ban(guild, member):
+    console.print("[red]{0.name} has been banned[/red]".format(member))
+    await member.send("You have been banned from the server!")
+
+@bot.event
+async def on_member_unban(guild, member):
+    console.print("[green]{0.name} has been unbanned[/green]".format(member))
+    await member.send("You have been unbanned from the server!")
 
 #Commands
 #List commands
