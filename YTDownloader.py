22352f5141e47150591bde4eb257a7aeb3a9b8df
joshua-noel <noelejoshua@gmail.com>
2022-05-25 23:16:51 -0400

    logging change

--- YTDownloader.py
+++ YTDownloader.py
@@ -50,4 +50,4 @@ class Downloader:
         else:     
             stream = yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
             stream.download(os.getcwd() + "/videos/", filename= "video.mp4")
-            console.print("[green]Downloaded a video[/green]")
+            console.log("[green]Downloaded a video[/green]")
