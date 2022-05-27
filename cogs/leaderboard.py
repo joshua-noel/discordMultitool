6d732e4cf3e4f0495cdb988782992e3772427a8e
joshua-noel <noelejoshua@gmail.com>
2022-05-27 19:03:14 -0400

    leaderboard ground work

---
+++ cogs/leaderboard.py
@@ -0,0 +1,32 @@
+import sys
+sys.dont_write_bytecode = True #Prevents creation of .pyc files
+import discord
+from discord.ext import commands, tasks
+import aiosqlite
+from rich.console import Console
+
+console = Console() #Rich console
+
+class Leaderboard(commands.Cog):
+    def __init__ (self, bot):
+        self.bot = bot
+
+    @commands.command(name= "leaderboard", aliases= ["lb"])
+    async def leaderboard(self, ctx):
+        async with aiosqlite.connect('database.db') as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT user_id, balance FROM economy ORDER BY balance DESC") #Sorts by highest balance
+                leaderboard = await cursor.fetchmany(10) #Gets top 10
+                embed = discord.Embed(title= "Leaderboard", color= 0x00ff00) #Creates embed
+
+                for i in range(len(leaderboard)):
+                    user = self.bot.get_user(leaderboard[i][0]) #Gets user from user_id
+                    embed.add_field(name= f"{i+1}. {user.name}", value= f"${leaderboard[i][1]}", inline=False)
+
+                await ctx.send(embed=embed)
+            
+            await db.commit()
+
+def setup(bot):
+    bot.add_cog(Leaderboard(bot))
+    console.log("[bright_cyan]Leaderboard Cog loaded...[/bright_cyan]")
