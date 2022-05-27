ad0801f7947bc8d48ec23ce1ccdada74cb86696c
joshua-noel <noelejoshua@gmail.com>
2022-05-26 20:35:08 -0400

    bot wide file handling

--- main.py
+++ main.py
@@ -10,15 +10,36 @@ load_dotenv() #Loads the .env file
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix= "&", intents=intents)
 
+#Bot wide error handling
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("That command doesn't exist!")
+
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("You're missing a required argument!")
+
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You don't have permission to do that!")
+
+    elif isinstance(error, commands.CheckFailure):
+        await ctx.send("You don't have permission to do that!")
+
+    elif isinstance(error, commands.CommandOnCooldown):
+        await ctx.send("You're on cooldown!")
+        
+    else:
+        raise error
+
 #Cog loading/Unloading
-@bot.command
+@bot.command(name= "load")
 @commands.has_permissions(administrator= True)
-async def load(ctx, extension):
+async def _load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
 
-@bot.command
+@bot.command(name= "unload")
 @commands.has_permissions(administrator= True)
-async def unload(ctx, extension):
+async def _unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
 
 @bot.command(name= "reload")
@@ -27,9 +48,9 @@ async def _reload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     bot.load_extension(f'cogs.{extension}')
 
-@bot.command
+@bot.command(name= "reloadall")
 @commands.has_permissions(administrator= True)
-async def reloadall(ctx):
+async def _reloadall(ctx):
     for file in os.listdir('./cogs'):
         if file.endswith('.py'):
             bot.unload_extension(f'cogs.{file[:-3]}')
