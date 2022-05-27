615fc78709a992b399ca649eff9dd541fbd63120
joshua-noel <noelejoshua@gmail.com>
2022-05-26 20:17:06 -0400

    permissions check for un/loading cogs

--- main.py
+++ main.py
@@ -12,20 +12,24 @@ bot = commands.Bot(command_prefix= "&", intents=intents)
 
 #Cog loading/Unloading
 @bot.command
+@commands.has_permissions(administrator= True)
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
 
 @bot.command
+@commands.has_permissions(administrator= True)
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
 
 @bot.command(name= "reload")
+@commands.has_permissions(administrator= True)
 async def _reload(ctx, extension):    
     bot.unload_extension(f'cogs.{extension}')
     bot.load_extension(f'cogs.{extension}')
 
 @bot.command
-async def reloadAll(ctx):
+@commands.has_permissions(administrator= True)
+async def reloadall(ctx):
     for file in os.listdir('./cogs'):
         if file.endswith('.py'):
             bot.unload_extension(f'cogs.{file[:-3]}')
