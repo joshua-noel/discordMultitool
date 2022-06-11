884a1f76a568077a7d528306a47679596dad6db1
joshua-noel <noelejoshua@gmail.com>
2022-06-11 17:57:08 -0500

    starboard now displays attachments

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -8,8 +8,8 @@ import random
 class Starboard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.starboard = self.bot.get_channel(984356171181883412)
-        self.colors = [0xFF0000, 0xFF7F00, 0xFFFF00, 0x00FF00, 0x00FFFF, 0x0000FF, 0xFF00FF]
+        self.starboard = bot.get_channel(984637437882036314)
+        self.colors = [0xFF0000, 0xFF7F00, 0xFFFF00, 0x00FF00, 0x00FFFF, 0x0000FF, 0xFF00FF, 0xFAFAFA]
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
@@ -19,8 +19,9 @@ class Starboard(commands.Cog):
             member = payload.member
             reaction = get(message.reactions, emoji=payload.emoji.name)
 
-            if reaction and reaction.count >= 5:
+            if reaction and reaction.count >= 4:
                 starboardEmbed = discord.Embed(title=f"{message.author}", description=f"{message.content}", color=random.choice(self.colors))
+                starboardEmbed.set_image(url=message.attachments[0].url)
                 await self.starboard.send(embed=starboardEmbed)
 
 def setup(bot):
