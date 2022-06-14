80748f21a8636c68e88986b0465d5215dd2fe717
joshua-noel <noelejoshua@gmail.com>
2022-06-14 05:23:29 -0500

    checks for 200 response before attempting to send content

--- cogs/apis.py
+++ cogs/apis.py
@@ -16,32 +16,40 @@ class APIs(commands.Cog):
         Get the latest xkcd comic or a specific comic.
         """
         if comic is None:
-            comic = random.randint(1, 1700)
+            comic = random.randint(1, 2632)
             url = f'https://xkcd.com/{comic}/info.0.json'
 
             async with request('GET', url) as r:
-                data = await r.json()
+                if r.status == 200:
+                    data = await r.json()
+                    comicEmbed = discord.Embed(title=data['title'], description=data['alt'], color=0xFAFAFA)
+                    comicEmbed.set_image(url=data['img'])
+                    comicEmbed.set_footer(text=f'Comic #{comic}')
 
-            comicEmbed = discord.Embed(title=data['title'], description=data['alt'], color=0x00ff00)
-            comicEmbed.set_image(url=data['img'])
-            comicEmbed.set_footer(text=f'Comic #{comic}')
-            await ctx.send(embed=comicEmbed)
-        
+                    await ctx.send(embed=comicEmbed)
+
+                else:
+                    await ctx.send('An error occurred.')
+
         elif comic.lower() == 'latest':
             url = 'https://xkcd.com/info.0.json'
 
             async with request('GET', url) as r:
-                data = await r.json()
+                if r.status == 200:
+                    data = await r.json()
+                    comicEmbed = discord.Embed(title=data['title'], description=data['alt'], color=0xFAFAFA)
+                    comicEmbed.set_image(url=data['img'])
+                    comicEmbed.set_footer(text=f'Comic #{data["num"]}')
 
-            comicEmbed = discord.Embed(title=data['title'], description=data['alt'], color=0x00ff00)
-            comicEmbed.set_image(url=data['img'])
-            comicEmbed.set_footer(text=f'Comic #{data["num"]}')
-            await ctx.send(embed=comicEmbed)
+                    await ctx.send(embed=comicEmbed)
+                
+                else:
+                    await ctx.send('An error occurred.')
 
         elif comic.isdigit():
             comic = int(comic)
 
-            if comic < 1 or comic > 1700:
+            if comic < 1 or comic > 2632:
                 await ctx.send('Comic not found.')
                 return
 
@@ -49,12 +57,16 @@ class APIs(commands.Cog):
                 url = f'https://xkcd.com/{comic}/info.0.json'
 
                 async with request('GET', url) as r:
-                    data = await r.json()
+                    if r.status == 200:
+                        data = await r.json()
+                        comicEmbed = discord.Embed(title=data['title'], description=data['alt'], color=0xFAFAFA)
+                        comicEmbed.set_image(url=data['img'])
+                        comicEmbed.set_footer(text=f'Comic #{comic}')
 
-                comicEmbed = discord.Embed(title=data['title'], description=data['alt'], color=0x00ff00)
-                comicEmbed.set_image(url=data['img'])
-                comicEmbed.set_footer(text=f'Comic #{comic}')
-                await ctx.send(embed=comicEmbed)
+                        await ctx.send(embed=comicEmbed)
+
+                    else:
+                        await ctx.send('An error occurred.')
 
 def setup(bot):
     bot.add_cog(APIs(bot))
